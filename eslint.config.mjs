1aec9c1eacf1bc17266591a05fa3b7fa4a48a772
Kamiel Quatacker <kamiel.quatacker@gmail.com>
2026-02-21 13:27:27 +0100

    chore: update dependencies in package.json
    - Added swiper version 12.1.2
    - Upgraded eslint to version 10.0.0
    - Updated eslint-config-next to version 0.2.4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,11 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "quotes": ["error", "double", { "avoidEscape": true }]
+    }
+  }
 ]);
 
 export default eslintConfig;
